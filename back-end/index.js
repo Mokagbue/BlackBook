c489ef9be50c69de594dc2784d7d419ebae46a8a
Gothic Jellybean <gothicjellybean@Gothics-iMac.local>
2019-04-18 20:40:11 -0600

    database and frontend set up

---
+++ back-end/index.js
@@ -0,0 +1,29 @@
+console.log("Good morning, index.js is functioning just fine!!");
+
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const logger = require('morgan');
+
+const server = express();
+
+server.use(express.json());
+server.use(logger('combined'));
+server.use(cors());
+server.use(helmet());
+
+//routes
+// const noteRoutes = require('./routes/noteRoutes.js');
+// const userRoutes = require('./routes/userRoutes.js');
+
+// server.use('/api/notes', noteRoutes);
+// server.use('/api/users', userRoutes);
+
+//server tester message
+server.get('/', (req, res) => {
+    res.send('Working...working...working...');
+});
+
+//port setup
+const port = 8000;
+server.listen(port, () => console.log(`==API is applying tiger balm on port ${port}==`));
